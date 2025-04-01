2c18dd8182bde05b2ab73e5eef6af2ac0a9c421b
Ethan Prescott <54092194+TimmyMcPickles@users.noreply.github.com>
2025-04-01 01:28:21 -0500

    Initial commit

---
+++ src/fps_controller.gd
@@ -0,0 +1,114 @@
+extends CharacterBody3D
+
+#Code was copied from this tutorial: https://www.youtube.com/watch?v=ZJr2qUrzEqg
+#Need to analyse it further so I get what's happening
+#Basically source-like movement
+
+@export var look_sensitivity : float = 0.006
+@export var jump_velocity := 6.0
+@export var auto_bhop := true
+
+# Ground movement settings
+@export var walk_speed := 7.0
+@export var sprint_speed := 8.5
+@export var ground_accel := 14.0
+@export var ground_decel := 10.0
+@export var ground_friction := 6.0
+
+# Air movement settings. Dial in if needed
+@export var air_cap := .85
+@export var air_accel := 800.0
+@export var air_move_speed := 500.0
+
+var wish_dir := Vector3.ZERO
+
+func get_move_speed() -> float:
+	return sprint_speed if Input.is_action_just_pressed("sprint") else walk_speed
+
+func _ready():
+	for child in %WorldModel.find_children("*", "VisualInstance3D"):
+		child.set_layer_mask_value(1, false)
+		child.set_layer_mask_value(2, true)
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			rotate_y(-event.relative.x * look_sensitivity)# Turn left and right
+			%Camera3D.rotate_x(-event.relative.y * look_sensitivity)# Look up and down
+			%Camera3D.rotation.x = clamp(%Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90))# clamp up and down to 90 degrees only
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	var input_dir = Input.get_vector("left", "right", "up", "down").normalized()
+	wish_dir = self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y)
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
+			self.velocity.y = jump_velocity
+		_handle_ground_physics(delta)
+	else:
+		_handle_air_physics(delta)
+	
+	move_and_slide()
+
+func clip_velocity(normal: Vector3, overbounce: float, delta: float) -> void:
+	# This code lets surfing happen (for funsies)
+	var backoff := self.velocity.dot(normal) * overbounce
+	
+	if backoff >= 0: return
+	
+	var change := normal * backoff
+	self.velocity -= change
+	
+	var adjust := self.velocity.dot(normal)
+	if adjust < 0.0:
+		self.velocity -= normal * adjust
+
+func is_surface_too_steep(normal: Vector3) -> bool:
+	var max_slop_ang_dot = Vector3(0,1,0).rotated(Vector3(1.0,0,0), self.floor_max_angle).dot(Vector3(0,1,0))
+	if normal.dot(Vector3(0,1,0)) < max_slop_ang_dot:
+		return true
+	return false
+
+func _handle_air_physics(delta) -> void:
+	self.velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
+	
+	# Source/quake like air movement recipe
+	var cur_speed_in_wish_dir = self.velocity.dot(wish_dir)
+	# Wish speed (if wish_dir > 0 length) capped to air_cap
+	var capped_speed = min((air_move_speed * wish_dir).length(), air_cap)
+	var add_speed_till_cap = capped_speed - cur_speed_in_wish_dir
+	if add_speed_till_cap > 0:
+		var accel_speed = air_accel * air_move_speed * delta
+		accel_speed = min(accel_speed, add_speed_till_cap)
+		self.velocity += accel_speed * wish_dir
+	
+	# Comment this out if you dont want surfing
+	if is_on_wall():
+		if is_surface_too_steep(get_wall_normal()):
+			self.motion_mode = CharacterBody3D.MOTION_MODE_FLOATING
+		clip_velocity(get_wall_normal(), 1, delta) # Allows surfing
+
+func _handle_ground_physics(delta) -> void:
+	# Similar to air movement. Acceleration and friction on ground
+	var cur_speed_in_wish_dir = self.velocity.dot(wish_dir)
+	var add_speed_till_cap = get_move_speed() - cur_speed_in_wish_dir
+	if add_speed_till_cap > 0:
+		var accel_speed = ground_accel * delta * get_move_speed()
+		accel_speed = min(accel_speed, add_speed_till_cap)
+		self.velocity += accel_speed * wish_dir
+	
+	# Apply friction
+	var control = max(self.velocity.length(), ground_decel)
+	var drop = control * ground_friction * delta
+	var new_speed = max(self.velocity.length() - drop, 0.0)
+	if self.velocity.length() > 0:
+		new_speed /= self.velocity.length()
+	self.velocity *= new_speed
